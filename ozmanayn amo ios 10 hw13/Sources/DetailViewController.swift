e84734afdf1930122a8b32ceaf629dbf9882fc4a
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-07-13 17:26:53 +0300

    Setting CustomTableViewCell and DetailViewController.

--- ozmanayn amo ios 10 hw13/Sources/DetailViewController.swift
+++ ozmanayn amo ios 10 hw13/Sources/DetailViewController.swift
@@ -1,8 +1,50 @@
-//
-//  DetailViewController.swift
 //  ozmanayn amo ios 10 hw13
 //
 //  Created by Amahastla on 13.07.2023.
 //
 
-import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var model: Model? {
+        didSet {
+            guard let model = model else { return }
+            label.text = model.title
+        }
+    }
+    
+    // MARK: - UI Elements
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    // MARK: - Setups
+    
+    private func setupView() {
+        view.backgroundColor = .systemCyan
+    }
+
+    private func setupHierarchy() {
+        view.addSubview(label)
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
